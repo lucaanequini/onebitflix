9a099f2f6cb18841f95cc4c44297817df28471fc
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-03-21 22:57:04 -0300

    Customizing the dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,7 +3,7 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from '@adminjs/sequelize'
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { User, Course, Episode, Category } from "../models";
 import bcrypt from 'bcrypt'
 import { locale } from "./locale";
 
@@ -14,6 +14,22 @@ export const adminJs = new AdminJS({
     rootPath: '/admin',
     resources: adminJsResources,
     locale: locale,
+    dashboard: {
+        component: AdminJS.bundle('./components/Dashboard'),
+        handler: async (req, res, context) => {
+            const courses = await Course.count()
+            const episodes = await Episode.count()
+            const categories = await Category.count()
+            const standartUsers = await User.count({ where: { role: 'user' } })
+
+            res.json({
+                'Cursos': courses,
+                'Episódios': episodes,
+                'Categorias': categories,
+                'Usuários': standartUsers
+            })
+        }
+    },
     branding: {
         companyName: 'OneBitFlix',
         logo: '/logoOnebitflix.svg',
